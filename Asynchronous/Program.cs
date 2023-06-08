053cb4f7795628f1c3150b58f47e57dc63a8dcba
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-06-08 16:48:36 -0300

    Demonstração de funcionamento da Thread

--- Asynchronous/Program.cs
+++ Asynchronous/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Asynchronous
 {
@@ -6,7 +7,34 @@ namespace Asynchronous
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Iniciando o Redimensionador!");
+
+            //Note que aqui o "Redimensionar" está sem parenteses.
+            //A thread está apenas apontando para o método que estará sob seu efeito,
+            //não está chamando o método.
+            Thread thread = new Thread(Redimensionar);
+            thread.Start();
+
+
+            //Redimensionar();
+
+            System.Console.WriteLine("Tecle para fechar");
+            Console.ReadKey();
+
         }
+        
+        static void Redimensionar()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine(i);
+
+                //Com o TimeSpan não é preciso calcular os milissegundos.
+                //Nesse formato temos (horas, minutos, segundos)                
+                Thread.Sleep(new TimeSpan(0, 0, 3));
+            }
+        }
+
+
     }
 }
