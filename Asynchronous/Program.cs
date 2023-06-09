3ef499fcbf671abd4408f57d154e9f3c900277ec
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-06-08 22:08:35 -0300

    Projeto OK

--- Asynchronous/Program.cs
+++ Asynchronous/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using System.Threading;
 
@@ -16,9 +17,7 @@ namespace Asynchronous
             Thread thread = new Thread(Redimensionar);
             thread.Start();
 
-
             Console.ReadKey();
-
         }
                 
         static void Redimensionar()
@@ -41,17 +40,67 @@ namespace Asynchronous
             {
                 Directory.CreateDirectory(diretorioFinalizados);
             }
-            #endregion            
+            #endregion 
 
-            /*while(true)
+            //Nova altura da imagem em pixels
+            int tamanhoAltura = 200;
+
+            while(true)
             {
+                //lendo ArquivosEntrada
+                var arquivosEntrada = Directory.EnumerateFiles(diretorioEntrada);
+
+                foreach (var arquivo in arquivosEntrada)
+                {
+                    FileStream fileStr = new FileStream(arquivo, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+                    FileInfo fileInf = new FileInfo(arquivo);
+
+                    //Diretório Local(pasta atual)\Pasta de Destino\Nome do Arquivo Original
+                    string caminho = Environment.CurrentDirectory + @"\" + diretorioRedimensionados + @"\" + fileInf.Name;
+
+                    //Redimensionando
+                    Redimensionador(Image.FromStream(fileStr), tamanhoAltura, caminho);
+
+                    //FECHA o arquivo para liberar a memória
+                    fileStr.Close();
+
+                    //Mover os arquivos originais para a pasta Finalizados
+                    string caminhoFinalizados = Environment.CurrentDirectory + @"\" + diretorioFinalizados + @"\" + fileInf.Name;
+                    fileInf.MoveTo(caminhoFinalizados);
+                }
 
                 //Com o TimeSpan não é preciso calcular os milissegundos.
                 //Nesse formato temos (horas, minutos, segundos)                
-                Thread.Sleep(new TimeSpan(0, 0, 3));
-            }*/
+                Thread.Sleep(new TimeSpan(0, 0, 5));
+            }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="imagem">Imagem Original a ser redimensionada</param>
+        /// <param name="altura">Altura que desejamos redimensionar</param>
+        /// <param name="caminho">caminho aonde será salva a nova imagem redimencionada</param>
+        /// <returns></returns>
+        static void Redimensionador(Image imagem, int altura, string caminho)
+        {
+            //(double) converte explicitamente os valores para double
+            double ratio = (double)altura / imagem.Height;
+            int novaLargura = (int)(imagem.Width * ratio);
+            int novaAltura = (int)(imagem.Height * ratio);
 
+            Bitmap novaImage = new Bitmap(novaLargura, novaAltura);
+
+            //Desenhando a imagem com os novos parametros
+            using(Graphics grafico = Graphics.FromImage(novaImage))
+            {
+                grafico.DrawImage(imagem, 0, 0, novaLargura, novaAltura);
+            }
+            
+            novaImage.Save(caminho);
+
+            //liberando toda a memória usada no processo de criação da nova imagem
+            imagem.Dispose();
+        }
     }
 }
